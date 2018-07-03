9a9feb64b00efb175edc28edf156a8ba7ac2707b
Khorevaa <Khorevaa@gmail.com>
2018-07-03 18:22:55 +0300

    Файлы настройки и сборки приложения

--- travis-ci.sh
+++ travis-ci.sh
@@ -1,18 +1,17 @@
-
-
-set -e
 echo "Устанавливаю версию OScript <$OSCRIPT_VERSION>"
 curl http://oscript.io/downloads/$OSCRIPT_VERSION/deb > oscript.deb 
 dpkg -i oscript.deb 
 rm -f oscript.deb
-
+echo "==================================="
+echo "Установка зависимостей тестирования"
 opm install 1testrunner; 
-opm install 1bdd; 
-opm install notify;
+opm install 1bdd;
 opm install coverage;
+
 opm install; 
 
+# cat /usr/bin/opm
+echo "==================================="
 
-opm run coverage; 
-
-
+echo "Запуск тестирования пакета"
+oscript ./tasks/coverage.os; 
