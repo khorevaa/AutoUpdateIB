05a1648a721443ad5dd1d42c40d09978417b66bc
Khorevaa <Khorevaa@gmail.com>
2018-07-05 16:55:49 +0300

    Установка rabbitmq запуск перед тестами

---
+++ start-rabbitmq.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+docker run -d --hostname rabbit --name rabbit -p 8080:15672 rabbitmq:3-management
+
+# ./wait-for-it.sh localhost:8080 
+# -t 30
+sleep 10
+
+until curl -i -u guest:guest http://localhost:8080/api/healthchecks/node >/dev/null ; do
+
+  >&2 echo "rabbitmq is unavailable - sleeping"
+
+  sleep 5
+
+done
+
+curl -i -u guest:guest -H "content-type:application/json" \
+    -XPUT -d'{"auto_delete":false,"durable":true}' \
+    http://localhost:8080/api/queues/%2f/all.update
+
+curl -i -u guest:guest -H "content-type:application/json" \
+    -XPUT -d'{"auto_delete":false,"durable":true}' \
+    http://localhost:8080/api/queues/%2f/report.update
