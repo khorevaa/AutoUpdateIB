2716f52307b055371f8fd6f1d70f6501b9dae663
Khorevaa <Khorevaa@gmail.com>
2018-07-06 10:26:41 +0300

    Корректное прокидывание rabbit

--- start-rabbitmq.sh
+++ start-rabbitmq.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-docker run -d --hostname rabbit --name rabbit -p 8080:15672 rabbitmq:3-management
+docker run -d --hostname rabbit --name rabbit -p 15672:15672 rabbitmq:3-management
 
 # ./wait-for-it.sh localhost:8080 
 # -t 30
 sleep 10
 
-until curl -i -u guest:guest http://localhost:8080/api/healthchecks/node >/dev/null ; do
+until curl -i -u guest:guest http://localhost:15672/api/healthchecks/node >/dev/null ; do
 
   >&2 echo "rabbitmq is unavailable - sleeping"
 
@@ -15,8 +15,8 @@ done
 
 curl -i -u guest:guest -H "content-type:application/json" \
     -XPUT -d'{"auto_delete":false,"durable":true}' \
-    http://localhost:8080/api/queues/%2f/all.update
+    http://localhost:15672/api/queues/%2f/all.update
 
 curl -i -u guest:guest -H "content-type:application/json" \
     -XPUT -d'{"auto_delete":false,"durable":true}' \
-    http://localhost:8080/api/queues/%2f/report.update
+    http://localhost:15672/api/queues/%2f/report.update
