93ac08522ea049c2953ff56467cf291e95c67e66
Khorevaa <Khorevaa@gmail.com>
2018-07-11 13:24:34 +0300

    Запуск artifactory перед тестированием

---
+++ start-artifactory.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+docker run -d --hostname artifactory --name artifactory -p 8081:8081 docker.bintray.io/jfrog/artifactory-oss
+
+sleep 25
+
+until curl -i -u admin:password http://localhost:8081/api/ >/dev/null ; do
+
+  >&2 echo "artifactory is unavailable - sleeping"
+
+  sleep 5
+
+done
+
+curl -i -u admin:password -T ./tests/fixtures/distr/1.0/1Cv8.cf  \
+     http://localhost:8081/artifactory/example-repo-local/test/cf/10/1.0/1Cv8.cf
+
+curl -i -u admin:password -T ./tests/fixtures/distr/1.1/1cv8.cf  \
+     http://localhost:8081/artifactory/example-repo-local/test/cf/10/1.1/1cv8.cf
+
+curl -i -u admin:password -T ./tests/fixtures/distr/1.1/1cv8.cfu  \
+     http://localhost:8081/artifactory/example-repo-local/test/cf/10/1.1/1cv8.cfu
