1fce3b5ffabf96a7be13e0f5faaf5a25b1c48dde
khorevaa <khorevaa@gmail.com>
2019-02-20 18:39:16 +0300

    Небольшие вспомогательные объекты

--- test-agent.bat
+++ test-agent.bat
@@ -1,1 +1,1 @@
-oscript.exe .\src\cmd\AutoUpdateIB.os -v --log-file .\agent.log agent -u guest -p guest -q all.update -P 15672 -t 30 localhost
+@oscript.exe .\src\cmd\AutoUpdateIB.os --log-file .\agent.log agent -u guest -p guest -q all.update --report-queue report.update -P 15672 -t 30 http://localhost
